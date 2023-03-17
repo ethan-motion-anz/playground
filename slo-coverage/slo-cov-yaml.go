348865d802a6e861544c290e3887830615f89938
Ethan Motion <ethan.motion@anz.com>
2023-03-17 16:57:16 +1100

    Give up on alignment

--- slo-coverage/slo-cov-yaml.go
+++ slo-coverage/slo-cov-yaml.go
@@ -15,50 +15,50 @@ import (
 */
 
 type SLOs struct {
-    SLO[]				 SLO 		`yaml:"slos"`
+    SLO[] SLO `yaml:"slos"`
 }
 
 type SLO struct {
-    Metadata 						`yaml:"metadata"`
-    Spec 							`yaml:"spec"`
+    Metadata `yaml:"metadata"`
+    Spec `yaml:"spec"`
 }
 
 // Nested under SLO
 type Metadata struct {
-    DisplayName 		string 		`yaml:"displayName"`
-    Name 				string 		`yaml:"name"`
-    Project 			string 		`yaml:"project"`
-    Label 							`yaml:"labels"`
+    DisplayName string `yaml:"displayName"`
+    Name string `yaml:"name"`
+    Project string `yaml:"project"`
+    Label `yaml:"labels"`
 }
 
 // Nested under SLO
 type Spec struct {
-    AlertPolicies[] 	string 		`yaml:"alertPolicies"`
-    Description 		string 		`yaml:"description"`
-    Attachments 					`yaml:"attachments"`
+    AlertPolicies[] string `yaml:"alertPolicies"`
+    Description string `yaml:"description"`
+    Attachments `yaml:"attachments"`
 }
 
 // Nested under Metadata
 type Label struct {
-    AnzxTechAsset[] 	string 		`yaml:"anz-x-tech-asset"`
-    AnzxValueStream[] 	string 		`yaml:"anz-x-value-stream"`
-    Component[] 		string 		`yaml:"component"`
-    DeployedFrom[] 		string 		`yaml:"deployed-from"`
-    SloType[] 			string 		`yaml:"slo-type"`
-    TeamName[] 			string 		`yaml:"team-name"`
-    UserJourney[] 		string 		`yaml:"user-journey"`
+    AnzxTechAsset[] string `yaml:"anz-x-tech-asset"`
+    AnzxValueStream[] string `yaml:"anz-x-value-stream"`
+    Component[] string `yaml:"component"`
+    DeployedFrom[] string `yaml:"deployed-from"`
+    SloType[] string `yaml:"slo-type"`
+    TeamName[] string `yaml:"team-name"`
+    UserJourney[] string `yaml:"user-journey"`
 }
 
 // Nested under Spec
 type Attachments[] struct {
-    DisplayName 		string 		`yaml:"displayName"`
-    URL 				string 		`yaml:"url"`
+    DisplayName string `yaml:"displayName"`
+    URL string `yaml:"url"`
 }
 
 // Used for created slices of components
 type Component struct {
-    ComponentName 		string
-    ComponentCount		int
+    ComponentName string
+    ComponentCount int
 }
 
 func readYamlFile (file_path string) ([]byte) {
